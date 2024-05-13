34f452c2a8ba4a4efa0aa25865f2e8fef6f4969f
alex vamv <vamvas@outlook.com.gr>
2024-05-13 22:34:13 +0100

    Create application factory function

---
+++ flaskr/__init__.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    # initialise Flask object
+    app = Flask(__name__, instance_relative_config = True)
+
+    # default configuration
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+
+    if test_config is None:
+        # override default configuration when not testing
+        app.config.from_pyfile('config.py', silent = True)
+    else:
+        # load test config, when testing
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # create a simple page
+    @app.route('/hello')
+    def hello():
+        return "Hello, Maria"
+    
+    return app
